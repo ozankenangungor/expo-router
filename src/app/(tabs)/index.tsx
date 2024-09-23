50d6ffa9960ae312263ff16e081288ae9f775dc8
ozankenangungor <s220204044@ankarabilim.edu.tr>
2024-09-23 07:31:09 +0300

    Create Post UI

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+
 import PostListItem from "@/src/components/PostListItem";
 import { FlatList, View } from "react-native";
 import posts from "@/assets/data/posts.json"
@@ -6,16 +7,9 @@ import posts from "@/assets/data/posts.json"
 
 export default function FeedScreen() {
     return (
-      <FlatList data={posts} 
+      <FlatList data={posts} renderItem={({item}) => <PostListItem post={item} />} 
+      contentContainerStyle={{gap: 10, maxWidth:512, width:'100%', alignSelf:'center'}} 
       showsVerticalScrollIndicator={false} 
-      contentContainerStyle={{gap: 10, maxWidth:512, width:'100%'}} 
-      renderItem={({item}) => {
-         return (
-         <View className="items-center">
-         <PostListItem post={item} />
-         </View>
-    )
-    }}
     />
     )
    
--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,9 +1,31 @@
-import { Text } from "react-native";
-
+import { Text, View, Image, TextInput, Pressable } from 'react-native';
+import { useState } from 'react';
 
 export default function CreatePost() {
+  const [caption, setCaption] = useState('');
 
-    return (
-        <Text>Create Post</Text>
-    )
+  return (
+    <View className="p-3 items-center flex-1">
+      {/* Image picker */}
+      <Image source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',}}
+        className="w-52 aspect-[3/4] rounded-lg bg-slate-300"
+      />
+
+      <Text onPress={() => {}} className="text-blue-500 font-semibold m-5">
+        Change
+      </Text>
+
+      {/* TextInput for caption */}
+      <TextInput value={caption} placeholder="What is on your mind" onChangeText={(newValue) => setCaption(newValue)}
+        className="w-full p-3"
+      />
+
+      {/* Button */}
+      <View className="mt-auto w-full">
+        <Pressable className="bg-blue-500 w-full p-3 items-center rounded-md">
+          <Text className="text-white font-semibold">Share</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
